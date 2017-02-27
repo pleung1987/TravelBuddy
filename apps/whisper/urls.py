59ff9b5af905e916ee06e5aee2d16c4c417c03f9
Patrick Leung <pleung1987@gmail.com>
2017-02-28 06:23:16 +0800

    Travel Buddy Completed

---
+++ apps/whisper/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^register$', views.register),
+    url(r'^login$', views.login),
+    url(r'^travel$', views.travel),
+    url(r'^addplan$', views.addplan),
+    url(r'^createplan$', views.createplan),
+    url(r'^show/(?P<travel_id>\d+)$', views.show),
+    url(r'^join/(?P<travel_id>\d+)$', views.join),
+    # url(r'^popular$', views.popular),
+    url(r'^logout$', views.logout),
+    url(r'^delete/(?P<id>\d+)$', views.delete)
+    # url(r'^delete/(?P<id>\d+)$', views.delete),
+]
